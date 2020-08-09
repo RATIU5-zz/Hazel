22230c0251dafe405719e9b8b42707c9fdeaa207
johnratius <jasonmemm@gmail.com>
2020-08-08 21:15:10 -0600

    Setup Log.h, EntryPoint.h, and premake5.lua

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Hazel/Application.h"
+#include "Hazel/Log.h"
+
+#include "Hazel/EntryPoint.h"
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+int main(int argc, char** argv)
+{
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_INFO("Hello Hazel!");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
